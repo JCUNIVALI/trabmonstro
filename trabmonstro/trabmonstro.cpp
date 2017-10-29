2bfa2a82d88f84134a2d82b127f66367800c7bd8
jothbc <jothbc@gmail.com>
2017-10-29 10:39:42 -0200

    tem q tenta faze uma função pra move o monstro recursiva...

--- trabmonstro/trabmonstro.cpp
+++ trabmonstro/trabmonstro.cpp
@@ -2,10 +2,65 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
+#define TAMANHOMATRIZ 5
+using namespace std;
 
 
+void movemostro(int MatrizMonstro[TAMANHOMATRIZ][TAMANHOMATRIZ], int LinhaMonstro, int ColunaMonstro, int LinhaPersonagem, int ColunaPersonagem) {
+	if(LinhaMonstro<LinhaPersonagem && ColunaMonstro == ColunaPersonagem) {
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 0;
+		LinhaMonstro++;
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 1;
+	}
+	if(ColunaMonstro<ColunaPersonagem && LinhaMonstro == LinhaPersonagem) {
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 0;
+		ColunaMonstro++;
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 1;
+	}
+	if(LinhaMonstro>LinhaPersonagem && ColunaMonstro == ColunaPersonagem) {
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 0;
+		LinhaMonstro--;
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 1;
+	}
+	if(ColunaMonstro>ColunaPersonagem && LinhaMonstro == LinhaPersonagem) {
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 0;
+		ColunaMonstro--;
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 1;
+	}
+	if(LinhaMonstro<LinhaPersonagem && ColunaMonstro<ColunaPersonagem) {
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 0;
+		LinhaMonstro++;
+		ColunaMonstro++;
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 1;
+	}
+	if(LinhaMonstro>LinhaPersonagem && ColunaMonstro<ColunaPersonagem) {
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 0;
+		LinhaMonstro--;
+		ColunaMonstro++;
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 1;
+	}
+	if(LinhaMonstro<LinhaPersonagem && ColunaMonstro>ColunaPersonagem) {
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 0;
+		LinhaMonstro++;
+		ColunaMonstro--;
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 1;
+	}
+	if(LinhaMonstro>LinhaPersonagem && ColunaMonstro>ColunaPersonagem) {
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 0;
+		LinhaMonstro--;
+		ColunaMonstro--;
+		MatrizMonstro[LinhaMonstro][ColunaMonstro] = 1;
+	}
+}
 int main()
 {
+	int MatrizPesonagem[TAMANHOMATRIZ][TAMANHOMATRIZ] = { 0 }, MatrizMonstro[TAMANHOMATRIZ][TAMANHOMATRIZ] = { 0 };
+	MatrizPesonagem[2][2] = 1;
+	MatrizMonstro[0][1] = 1;
+	MatrizMonstro[3][4] = 1;
+	MatrizMonstro[4][0] = 1;
+	MatrizMonstro[4][2] = 1;
     return 0;
 }
 
